bbb66dcf6801541a8e11d27e2c0360ccfc7d31fa
Jason Mitchell <devjmitchell@gmail.com>
2024-07-24 10:21:08 -0400

    Initial Commit

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Jason Mitchell on 7/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
